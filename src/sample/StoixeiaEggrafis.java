c9dabb7d44d120661f7fb0546e29aaea5b8a0a6d
gsak3l <gsakel5@gmail.com>
2019-10-28 19:25:20 +0200

    first commit

---
+++ src/sample/StoixeiaEggrafis.java
@@ -0,0 +1,15 @@
+package sample;
+
+public class StoixeiaEggrafis {
+    private int akadimaikoEtosEggrafis;
+    private String troposEggrafis;
+    private int eksaminoEggrafis;
+    private String periodosEggrafis;
+
+    public StoixeiaEggrafis(int akadimaikoEtosEggrafis, String troposEggrafis, int eksaminoEggrafis, String periodosEggrafis) {
+        this.akadimaikoEtosEggrafis = akadimaikoEtosEggrafis;
+        this.troposEggrafis = troposEggrafis;
+        this.eksaminoEggrafis = eksaminoEggrafis;
+        this.periodosEggrafis = periodosEggrafis;
+    }
+}
---
+++ src/sample/VathmologiaFoititi.java
@@ -0,0 +1,15 @@
+package sample;
+
+class VathmologiaFoititi {
+    private Foititis foititis;
+    private Mathimata mathima;
+    private float vathmos;
+
+    public VathmologiaFoititi(Foititis foititis, Mathimata mathima, float vathmos) {
+        this.foititis = foititis;
+        this.mathima = mathima;
+        this.vathmos = vathmos;
+    }
+
+
+}
